14ad1baecc1925dad7e561196cc77712e6081b99
Randy <rachana@educacion.navarra.es>
2023-11-16 20:10:01 +0100

    conexion

---
+++ conexion  a Base de Datos/App.xaml.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace conexion__a_Base_de_Datos
+{
+    /// <summary>
+    /// Lógica de interacción para App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
